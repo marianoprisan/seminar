854aa712262b106bfb88a55d6756d97049bfa10d
lvl <marian.razvan.oprisan@gmail.com>
2016-11-07 21:09:05 +0000

    Added to test branch

--- file.c
+++ file.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
 int main(void){
-    printf("Hello, world");
+    printf("Hello, world\n");
+
+    //This is added into the latest branch
+    printf("This line is displayed only on the test branch\n");
     return 0;
 }
